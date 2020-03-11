facea6080350b4d39d23a1031c922d76c44ae654
Ioannis Leontiadis <giannisleondiadis@gmail.com>
2020-03-11 10:54:02 +0200

    Create models for logger

--- eve/logger/models.py
+++ eve/logger/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    PROGRAMMING = 'PGM'
+    MATHEMATICS = 'MTH'
+    PHYSICS = 'PHY'
+    CIRCUITS_AND_ELECTRONICS = 'CAE'
+    CATEGORY_CHOICES = [
+            (PROGRAMMING, 'Programming'),
+            (MATHEMATICS, 'Mathematics'),
+            (PHYSICS, 'Physics'),
+            (CIRCUITS_AND_ELECTRONICS, 'Circuits and Electronics')
+    ]
+
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    category = models.CharField(
+            max_length=3,
+            choices=CATEGORY_CHOICES,
+    )
+
+class Entry(models.Model):
+    name = models.CharField(max_length=200)
+    duration = models.DurationField()
+    notes = models.TextField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
